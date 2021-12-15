e8dfcc5b0aa8af9ba819db777b4b891394a343a8
KevinDanae <kevin.danaea@gmail.com>
2021-12-14 20:36:53 -0600

    Creacion de entryviews y no selected

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="entry-container mb-3 pointer p-2">
+  <div
+    class="entry-container mb-3 pointer p-2"
+    @click="$router.push({ name: 'entry', params: { entryId: 10 } })"
+  >
     <!-- Titulo -->
     <div class="entry-title d-flex">
       <span class="text-success fs-5 fw-bold">13</span>
--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -1,7 +1,16 @@
 export default {
-    name: 'daybook',
-    component: () => import('@/modules/daybook/layouts/DaybookLayout.vue'),
-    children: [
-        
-    ]
-}
+  name: "daybook",
+  component: () => import("@/modules/daybook/layouts/DaybookLayout.vue"),
+  children: [
+    {
+      path: "",
+      name: "no-entry",
+      component: () => import("@/modules/daybook/views/NoEntrySelected.vue"),
+    },
+    {
+      path: ":entryId",
+      name: "entry",
+      component: () => import("@/modules/daybook/views/EntryView.vue"),
+    }
+  ],
+};
